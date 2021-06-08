703bc1ee7462d1c772129f46ceb6b67ff11f028e
Moriah Taylor <moriahataylor@gmail.com>
2021-06-08 12:45:33 -0400

    add filter capability

--- static/js/app.js
+++ static/js/app.js
@@ -6,24 +6,43 @@ console.log(tableData)
 
 var tbody = d3.select("tbody");
 
-//tableData.forEach((UFOSighting) => {
-//    var row = tbody.append("tr");
-//    Object.entries(UFOSighting).forEach(([key, value]) => {
-//        var cell = row.append("td");
-//        cell.text(value)
-//    });
-//});
-
-var UFO_date = d3.select("#datetime").text();
-
-function isDate(datetime){
-    return (datetime===UFO_date)
-}
-
-tableData.filter(isDate).forEach((UFOSighting) => {
+tableData.forEach((UFOSighting) => {
     var row = tbody.append("tr");
     Object.entries(UFOSighting).forEach(([key, value]) => {
         var cell = row.append("td");
-        cell.text(value)
+        cell.text(value);
+    });
+});
+
+var button = d3.select("#filter-btn");
+
+button.on("click", function(){
+    tbody.html("");
+    // select html input
+    var inputHTML = d3.select("#datetime");
+    // get value property
+    var inputValue = inputHTML.property("value");
+    // log value
+    console.log(inputValue);
+    // filter to selected date
+    var filteredSightings = tableData.filter(sighting => sighting.datetime === inputValue);
+    // log filtered values
+    console.log(filteredSightings)
+
+    // populate data in table
+    filteredSightings.forEach(function(selectedSighting){
+        // log sighting
+        console.log(selectedSighting);
+        // add row to table
+        var row = tbody.append("tr");
+        // use `Object.entries` to console log each value and append to table
+        Object.entries(selectedSighting).forEach(function([key, value]){
+            // log key and value
+            console.log(key, value);
+            // append cell to row
+            var cell = row.append("td");
+            // populate cell with value
+            cell.text(value);
+        });
     });
 });
